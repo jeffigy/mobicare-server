ad72b03a2e2f8f16436bbc11442c1ba7d30affea
jeffigy <ejeffydev@gmail.com>
2024-10-14 14:27:50 +0800

    feat: Created new user function, and route

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,7 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+const sendVerificationEmail = require("../utils/sendVerficationEmail");
 
 const getAllUsers = async (req, res) => {
   const users = await User.find({}).exec();
@@ -8,5 +11,42 @@ const getAllUsers = async (req, res) => {
 
   res.json(users);
 };
+const newUser = async (req, res) => {
+  const { name, email, password, roles } = req.body;
 
-module.exports = { getAllUsers };
+  if (!name || !email || !password || !roles.length) {
+    return res
+      .status(400)
+      .json({ message: "Name, email, and password are required" });
+  }
+
+  const duplicateEmail = await User.findOne({ email })
+    .collation({ locale: "en", strength: 2 })
+    .lean()
+    .exec();
+
+  if (duplicateEmail) {
+    return res.status(409).json({ message: "Email already exists" });
+  }
+
+  const hashedPwd = await bcrypt.hash(password, 10);
+  const verificationToken = crypto.randomBytes(32).toString("hex");
+  const verificationTokenExpiration = Date.now() + 24 * 60 * 60 * 1000;
+
+  const newUser = await User.create({
+    name,
+    email,
+    password: hashedPwd,
+    verificationToken,
+    verificationTokenExpiration,
+    roles,
+  });
+
+  await sendVerificationEmail(newUser.email, verificationToken);
+
+  res.status(201).json({
+    message: `A verification email has been sent to ${newUser.email}.`,
+  });
+};
+
+module.exports = { getAllUsers, newUser };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const userRoutes = require("express").Router();
-const { getAllUsers } = require("../controllers/userController");
+const { getAllUsers, newUser } = require("../controllers/userController");
 const verifyJWT = require("../middleware/verifyJWT");
 
 userRoutes.use(verifyJWT);
 
-userRoutes.route("/").get(getAllUsers);
+userRoutes.route("/").get(getAllUsers).post(newUser);
 
 module.exports = userRoutes;
