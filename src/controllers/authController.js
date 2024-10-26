e262a456dede0008139cf4ef642f51f61e06cc1e
jeffigy <ejeffydev@gmail.com>
2024-10-27 02:16:45 +0800

    fix: Fixed deployment issue 9

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -53,6 +53,8 @@ const verifyEmail = async (req, res) => {
     secure: true,
     sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000,
+    path: "/",
+    domain: ".onrender.com",
   });
 
   res.json({ accessToken });
@@ -131,6 +133,8 @@ const login = async (req, res) => {
     secure: true,
     sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000,
+    path: "/",
+    domain: ".onrender.com",
   });
 
   res.json({ accessToken });
